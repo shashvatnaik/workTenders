2435f1e49042af03c13b430c07906cfad916ab9d
shashvat <shashvatzm@gmail.com>
2019-08-09 00:11:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', function (req, res) {
+    res.send('<h1>Work Tenders</h1>');
+ })
+ 
+ var server = app.listen(port, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("app listening at http://%s:%s", host, port)
+ })
