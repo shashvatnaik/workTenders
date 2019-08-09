123a27ff72b1bc4b6f5f75af9fb6a7c511df5475
shashvat <shashvatzm@gmail.com>
2019-08-09 21:51:11 +0530

    changes

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
-    res.send('<h1>Work Tenders</h1>');
- })
+    res.send('<h1>Work Tenders!</h1>');
+ });
  
  var server = app.listen(port, function () {
     var host = server.address().address
     var port = server.address().port
     
     console.log("app listening at http://%s:%s", host, port)
- })
+ });
