23530dd1c72fec4060c0857fdfeb2557eab076b5
shashvat <shashvatzm@gmail.com>
2019-08-09 21:52:21 +0530

    removed exclamtion

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
-    res.send('<h1>Work Tenders!</h1>');
+    res.send('<h1>Work Tenders</h1>');
  });
  
  var server = app.listen(port, function () {
